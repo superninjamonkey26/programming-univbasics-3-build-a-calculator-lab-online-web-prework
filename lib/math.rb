5404ec987b15be4e76457a65dd800393cc80004a
Monkey Mendel <stephanie.mendel26@gmail.com>
2019-10-27 21:24:00 +0000

    Automatically backed up by Learn

--- lib/math.rb
+++ lib/math.rb
@@ -10,17 +10,14 @@ def division num1, num2
   p num1 / num2
 end
 
-#multiplication multiplies 2 numbers together
 def multiplication num1, num2
   p num1 * num2
 end
 
-#modulo gives the remainder
 def modulo num1, num2
   p num1.modulo(num2)
 end
 
-#suare root gives the square root of 2 numbers
-def square_root(num)
-
+def square_root num
+  p Math.sqrt num
 end

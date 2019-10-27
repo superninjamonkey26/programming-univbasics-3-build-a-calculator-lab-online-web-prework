c551669d4971b4e292330d189610b5e4070fcca7
Monkey Mendel <stephanie.mendel26@gmail.com>
2019-10-27 21:13:00 +0000

    Automatically backed up by Learn

--- lib/math.rb
+++ lib/math.rb
@@ -1,21 +1,26 @@
-def addition(num1, num2)
-
+#addition adds 2 numbers together
+def addition num1, num2
+  p num1 + num2
 end
 
-def subtraction(num1, num2)
-
+#subtraction subtracts 2 numbers from each other
+def subtraction num1, num2
+  p num1 - num2
 end
 
-def division(num1, num2)
-
+#division divides 2 numbers from each other
+def division num1, num2
+  p num1 / num2
 end
 
-def multiplication(num1, num2)
-
+#multiplication multiplies 2 numbers together
+def multiplication num1, num2
+  p num1 * num2
 end
 
-def modulo(num1, num2)
-
+#modulo gives the remainder
+def modulo num1, num2
+  p num1 () num2
 end
 
 def square_root(num)
